f245b8f682e6c3a3b572775e66822c4d8125f8aa
LunguGeorgeProgramator <51510106+LunguGeorgeProgramator@users.noreply.github.com>
2019-06-09 12:01:58 +0300

    set up css style for the guide popUp form, front end

--- player.js
+++ player.js
@@ -8,7 +8,6 @@ fetch('guide_v.json')
 .then(function(json) {
   var jsonStep = json.steps;
   for (var i in jsonStep) {
-	  console.log(i);
 	  $(jsonStep[i]['selector']).attr('step_attr', jsonStep[i]['id']);
 	  $(jsonStep[i]['selector']).click(function() {
 		  popUpGuide($(this), jsonStep);
@@ -24,15 +23,17 @@ function popUpGuide(target, json) {
 	var nextElement = null;
 
 	const curentPopUpOptions = findComponent(json, idTarget);
+	if(curentPopUpOptions === null){
+		throw new Error("  " + response.status);
+	}
 	const nextPopUpOptions = findComponent(json, curentPopUpOptions['next']);
 	
 	if(nextPopUpOptions !== null){
 		nextElement = nextPopUpOptions['selector'];
 	}
-	
 	// construct new popUp 
-	const element = $('<section class="close-Tip" style="background: red" >'+curentPopUpOptions['content']+'<p class="close-div">close</p>  <p class="next-div" >apasa pe next</p></div>');
-	
+	const element = buildPopUpSection(curentPopUpOptions);
+	buildPopUpSection(element);
 	// close popUp button listener
 	element.find( ".close-div" ).click(function(){
 		element.remove();
@@ -53,6 +54,43 @@ function popUpGuide(target, json) {
 	target.after(element);
 }
 
+function buildPopUpSection(curentPopUpOptions){
+	var nextButton = curentPopUpOptions['next'] !== null? '<p style="width: 50%; float: right; border-radius:  0 0 10px;" class="button next-div" >next</p>' : "";
+	var withButton = curentPopUpOptions['next'] !== null? "50%; border-radius: 0 0 0 10px;" : "100%; border-radius: 0 0 10px 10px;";
+	const element = $('<section class="close-Tip"><p class="pop-up-text">'+curentPopUpOptions['content']+'</p><spam class="container-buttons"><p style="width: '+withButton+'  float: left;" class="button close-div">close</p>'+nextButton+'</spam></section>');
+	const button = element.find(".button");
+	
+	// add the css for the popUp container
+	element.find("p").css({
+		"padding": "0px",
+		"margin": "0px"
+	});
+	element.find(".pop-up-text").css({
+		"padding": "10px"
+	});	
+	element.find(".container-buttons").css({
+		"text-align": "center",
+		"position": "relative"
+	});
+	button.css({
+		"background-color": "cadetblue",
+		"cursor": "pointer"
+	});
+	element.css({
+	"background-color": "POWDERBLUE", 
+	"border-radius": "10px", 
+	});
+	
+	// set hover action on button
+	button.mouseenter(function(event) {
+		$(event.target).css({ "background-color": "cornflowerblue" });
+	}).mouseleave(function() { 
+		$(this).css({ "background-color": "cadetblue" });
+	});
+	
+	return element;
+}
+
 function findComponent(json, IdToMatch){
 	for (var i in json) {
 		if (IdToMatch == json[i]['id']){
@@ -61,4 +99,3 @@ function findComponent(json, IdToMatch){
 	}
 	return null;
 } 
-
